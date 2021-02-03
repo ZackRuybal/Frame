4269666fb4ebef9f4dad9091bb05000d7b7ad45d
Zack Ruybal <zack@zruybal.com>
2021-02-02 23:49:26 -0700

    CLI arguments
    There's now CLI arguments, mostly for myself. Options are: all, master, demo. I realized that when I moved away from PrePros that my demo CSS wasn't geting compiled, so overly complicated solution here we come.

--- compile.sh
+++ compile.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
+# Hi my name is Zack and I built a compile script when I probably could have automated this differently, nice to meet you.
+
 # To use this, you need SASS, PostCSS, and Autoprefixer.
 # NPM install: sudo npm install -g sass postcss-cli autoprefixer
 
-echo "Compiling..."
-sass css/master.scss:css/master.css --no-source-map || echo "Failed"
-echo "Compiled"
-echo ""
-echo "Compressing..."
-sass css/master.scss:css/master.min.css --no-source-map --style compressed || echo "Failed"
-echo "Compressed"
-echo ""
-echo "Prefixing..."
-postcss css/*.css --use autoprefixer -d css/ || echo "Failed"
-echo "Prefixed"
-echo ""
-echo "Done."
+# 
+if [ "$1" = "master" ]; then
+    echo "Compiling [master]..."
+    sass css/master.scss:css/master.css --no-source-map || echo "Failed"
+    echo "Compiled"
+    echo ""
+    echo "Compressing [master]..."
+    sass css/master.scss:css/master.min.css --no-source-map --style compressed || echo "Failed"
+    echo "Compressed"
+    echo ""
+    echo "Prefixing [master]..."
+    postcss css/*.css --use autoprefixer -d css/ || echo "Failed"
+    echo "Prefixed"
+    echo ""
+    printf "$(tput setaf 2)Done.\n$(tput setaf 7)" # Oooh fancy colors
+elif [ "$1" = "demo" ]; then
+    echo "Compressing & Compiling [demo]..."
+    sass demo.scss:demo.css --no-source-map --style compressed || echo "Failed"
+    echo "Compressed"
+    echo ""
+    echo "Prefixing [demo]..."
+    postcss demo.css --use autoprefixer -d ./ || echo "Failed"
+    echo "Prefixed"
+    echo ""
+    printf "$(tput setaf 2)Done.\n$(tput setaf 7)" # Oooh fancy colors
+elif [ "$1" = "all" ]; then
+    echo "Compiling [master]..."
+    sass css/master.scss:css/master.css --no-source-map || echo "Failed"
+    echo "Compiled"
+    echo ""
+    echo "Compressing [master]..."
+    sass css/master.scss:css/master.min.css --no-source-map --style compressed || echo "Failed"
+    echo "Compressed"
+    echo ""
+    echo "Prefixing [master]..."
+    postcss css/*.css --use autoprefixer -d css/ || echo "Failed"
+    echo "Prefixed"
+    printf "$(tput setaf 2)Done.\n\n\n$(tput setaf 7)" # Oooh fancy colors
+    echo "Compressing & compiling [demo]..."
+    sass demo.scss:demo.css --no-source-map --style compressed || echo "Failed"
+    echo "Compressed"
+    echo ""
+    echo "Prefixing [demo]..."
+    postcss demo.css --use autoprefixer -d ./ || echo "Failed"
+    echo "Prefixed"
+    printf "$(tput setaf 2)Done.\n$(tput setaf 7)" # Oooh fancy colors
+else
+    echo "$(tput setaf 1)Options: $(tput setaf 2)all, master, demo$(tput setaf 7)"
+fi
